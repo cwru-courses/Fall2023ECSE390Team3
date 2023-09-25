28cbc9597daf0c46c5ae2a4eb6397f2c9ad3d5bc
SamRoths <102557110+SamRoths@users.noreply.github.com>
2023-09-25 19:23:21 -0400

    Adds visual effects for enemy deaths
    after an enemy dies a puff of smoke along with a walking chicken appear, both slowly fade away.
    
    Note: for this to function make sure to add the smoke cloud and Post-Death-Chicken to the serialized field of the enemy

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public abstract class BaseEnemy : MonoBehaviour
 {
 	protected int health;
@@ -9,6 +10,9 @@ public abstract class BaseEnemy : MonoBehaviour
 	protected bool alive;
 	protected Rigidbody2D rb2d;
 	[SerializeField] protected GameObject playerObject;
+	[SerializeField] protected GameObject smokeCloudPrefab;
+	[SerializeField] protected GameObject postDeathEntityPrefab;
+	[SerializeField] protected float deathAnimLength;
 
 	void Start()
 	{
@@ -29,9 +33,45 @@ public abstract class BaseEnemy : MonoBehaviour
 		}
 	}
 
-	protected IEnumerator Die()
+	protected virtual IEnumerator Die()
 	{
-		yield return new WaitForSeconds(2.5f);
+		float timeToDestroy = 2.5f; // default value for the time objects which appear after death will last for
+
+		//wait before initiating smoke etc
+		yield return new WaitForSeconds(deathAnimLength);
+
+		//Create smoke cloud and post death animal to appear at the position of the enemy
+		GameObject smokeCloudObject = null;
+		GameObject postDeathEntityObject = null;
+		
+		//check if there is a prefab for the smoke cloud to instantiate
+        if (smokeCloudPrefab)
+        {
+			smokeCloudObject = Instantiate(smokeCloudPrefab) as GameObject;
+			smokeCloudObject.transform.position = transform.position;
+        }
+		
+		//check if there is a prefab for the post death entity
+		if (postDeathEntityPrefab)
+		{
+			postDeathEntityObject = Instantiate(postDeathEntityPrefab) as GameObject;
+			postDeathEntityObject.transform.position = transform.position;
+
+			//set time to destroy based on lifetime of post death object
+			PostDeathEntity postDeathEntityComponent = postDeathEntityObject.GetComponent<PostDeathEntity>();
+            if (postDeathEntityComponent) { timeToDestroy = postDeathEntityComponent.getLifetime(); }
+		}
+
+		//make this enemy invisible
+		SpriteRenderer SR = GetComponent<SpriteRenderer>();
+        if (SR != null) { SR.color = Color.clear; }
+
+		//wait for smoke and post death entity to do their thing
+		yield return new WaitForSeconds(timeToDestroy);
+
+		//destroy everything
+		Destroy(postDeathEntityObject);
+		Destroy(smokeCloudObject);
 		Destroy(this.gameObject);
 	}
 
