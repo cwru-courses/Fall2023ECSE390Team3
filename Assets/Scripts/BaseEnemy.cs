5dc80a8d1b7943c940751d827ce81d7f01811be0
SamRoths <102557110+SamRoths@users.noreply.github.com>
2023-09-26 09:17:46 -0400

    Fixed Merge conflicts
    Hopefully

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -2,38 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public abstract class BaseEnemy : MonoBehaviour
 {
-	protected int health;
-	protected int movementSpeed;
-	protected bool alive;
-	protected Rigidbody2D rb2d;
+	[SerializeField] protected LayerMask whatIsPlayer;
+	[SerializeField] protected int maxHealth;
+	[SerializeField] protected int movementSpeed;
 	[SerializeField] protected GameObject playerObject;
 	[SerializeField] protected GameObject smokeCloudPrefab;
 	[SerializeField] protected GameObject postDeathEntityPrefab;
 	[SerializeField] protected float deathAnimLength;
 
+	protected int health;
+	protected bool alive;
+	protected Rigidbody2D rb2d;
+
 	void Start()
 	{
-		
+		health = maxHealth;
+		alive = true;
+		rb2d = GetComponent<Rigidbody2D>();
 	}
 
-	public bool isAlive()
-	{
-		return alive;
-	}
+	public bool isAlive() { return alive; }
+
 	public virtual void ReactToHit(int damage)
 	{
 		health -= damage;
 		if (health <= 0)
 		{
 			alive = false;
+			StopAllCoroutines();
 			StartCoroutine(Die());
 		}
 	}
 
-	protected virtual IEnumerator Die()
+	protected IEnumerator Die()
 	{
 		float timeToDestroy = 2.5f; // default value for the time objects which appear after death will last for
 
@@ -43,14 +46,14 @@ public abstract class BaseEnemy : MonoBehaviour
 		//Create smoke cloud and post death animal to appear at the position of the enemy
 		GameObject smokeCloudObject = null;
 		GameObject postDeathEntityObject = null;
-		
+
 		//check if there is a prefab for the smoke cloud to instantiate
-        if (smokeCloudPrefab)
-        {
+		if (smokeCloudPrefab)
+		{
 			smokeCloudObject = Instantiate(smokeCloudPrefab) as GameObject;
 			smokeCloudObject.transform.position = transform.position;
-        }
-		
+		}
+
 		//check if there is a prefab for the post death entity
 		if (postDeathEntityPrefab)
 		{
@@ -59,12 +62,12 @@ public abstract class BaseEnemy : MonoBehaviour
 
 			//set time to destroy based on lifetime of post death object
 			PostDeathEntity postDeathEntityComponent = postDeathEntityObject.GetComponent<PostDeathEntity>();
-            if (postDeathEntityComponent) { timeToDestroy = postDeathEntityComponent.getLifetime(); }
+			if (postDeathEntityComponent) { timeToDestroy = postDeathEntityComponent.getLifetime(); }
 		}
 
 		//make this enemy invisible
 		SpriteRenderer SR = GetComponent<SpriteRenderer>();
-        if (SR != null) { SR.color = Color.clear; }
+		if (SR != null) { SR.color = Color.clear; }
 
 		//wait for smoke and post death entity to do their thing
 		yield return new WaitForSeconds(timeToDestroy);
