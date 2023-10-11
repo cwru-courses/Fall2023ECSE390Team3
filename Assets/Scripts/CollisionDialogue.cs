2b43432adc5d2f09d3e096bed244a49f27fa6783
anaperezz <102556053+anaperezz@users.noreply.github.com>
2023-10-11 02:37:39 -0400

    Implemented dialogue throughout the game

--- Assets/Scripts/CollisionDialogue.cs
+++ Assets/Scripts/CollisionDialogue.cs
@@ -8,16 +8,20 @@ public class CollisionDialogue : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public String[] lines;
-    public float textSpeed;
-    private int index;
+    protected float textSpeed;
+    protected int index;
+    private DefaultInputAction playerInputAction;
 
 
+    public bool isRunning = false;
 
-
+    private GameObject dialogueBox;
 
     // Start is called before the first frame update
-    void OnTriggerEnter2D()
+    public void StartRunning(GameObject inputDialogueBox)
     {
+        this.dialogueBox = inputDialogueBox;
+        isRunning = true;
         textComponent.text = string.Empty;
         StartDialogue();
     }
@@ -25,13 +29,15 @@ public class CollisionDialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            if(textComponent.text == lines[index]){
-                NextLine();
-            }
-            else{
-                StopAllCoroutines();
-                textComponent.text = lines[index];
+        if(isRunning){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                if(textComponent.text == lines[index]){
+                    NextLine();
+                }
+                else{
+                    StopAllCoroutines();
+                    textComponent.text = lines[index];
+                }
             }
         }
 
@@ -40,16 +46,28 @@ public class CollisionDialogue : MonoBehaviour
     void StartDialogue(){
         
         index = 0;
-
+        PlayerMovement._instance.OnPause(true);
+        PlayerAttack._instance.OnPause(true);
+        PhaseShift._instance.OnPause(true);
+        PlayerStats._instance.OnPause(true);
         StartCoroutine(TypeLine());
     
     }
 
-    IEnumerator TypeLine(){
+    public virtual IEnumerator TypeLine(){
         foreach(char c in lines[index].ToCharArray()){
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+    }
+
+    void endDialogue(){
+        isRunning=false;
+        dialogueBox.SetActive(false);
+        PlayerMovement._instance.OnPause(false);
+        PlayerAttack._instance.OnPause(false);
+        PhaseShift._instance.OnPause(false);
+        PlayerStats._instance.OnPause(false);
     }
 
     void NextLine(){
@@ -59,7 +77,7 @@ public class CollisionDialogue : MonoBehaviour
             StartCoroutine(TypeLine());
         }
         else{
-            gameObject.SetActive(false);
+            endDialogue();
         }
     }
 }
