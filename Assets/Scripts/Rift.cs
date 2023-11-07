75e35af1e63aebd74a0bfbfecade66e2aa96bc08
hartwilliams <129880020+hartwilliams@users.noreply.github.com>
2023-11-07 02:08:18 -0500

    Did rift script, just need to know how new input system works to finish it

---
+++ Assets/Scripts/Rift.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rift : MonoBehaviour {
+
+    // Player and collider of player
+    private GameObject player;
+    private Collider2D playerCollider;
+
+    // Collider of this rift game object
+    private Collider2D riftCollider;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        riftCollider = GetComponent<Collider2D>();
+        player = GameObject.Find("Player");
+        playerCollider = player.GetComponent<Collider2D>();
+    }
+
+    void Update() {
+
+    }
+
+    // If player makes contact with rift for 2 seconds, trigger shift/stitch
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            StartCoroutine(UseRift());
+        }
+    }
+
+    private IEnumerator UseRift() {
+        yield return new WaitForSeconds(2.0f);
+
+        if (playerCollider.IsTouching(riftCollider)) {
+            // trigger shift/stitch
+        }
+        else
+            yield return null;
+    }
+
+}
