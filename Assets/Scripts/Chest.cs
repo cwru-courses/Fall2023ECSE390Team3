b0e28c5a3defbce655c35a1c4969a0a419a03d66
hartwilliams <129880020+hartwilliams@users.noreply.github.com>
2023-11-14 02:30:07 -0500

    Camera focus not fully working but getting there

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour {
 
-    // [SerializeField] private *list / data structure for items*
     [SerializeField] private SpriteRenderer chest;
     [SerializeField] private Sprite openChest;
     [SerializeField] private Sprite closedChest;
@@ -13,8 +12,8 @@ public class Chest : MonoBehaviour {
     [SerializeField] protected int yarnDroprate;
     [SerializeField] private AudioSource audSource;
     protected bool lootDropped = false;
-
     // [SerializeField] private Animation anim;
+    [SerializeField] private Camera mainCamera;
 
     // Start is called before the first frame update
     void Wake() {
@@ -23,35 +22,54 @@ public class Chest : MonoBehaviour {
         audSource.mute = false;
 
         // anim = gameObject.GetComponent<Animation>();
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
 
     // Set the sprite to an open chest or a closed chest
-    public void setChestOpen(bool boolean) { 
+    public void setChestOpen(bool boolean) {
+        // Start coroutine to open chest
         if (boolean == true) {
-            
-            audSource.Play();
-
-            chest.sprite = openChest;
-            // anim.Play("Chest_open");
-            
-            if (lootDropped == false)
-            {
-                Instantiate(lootSpawnerPrefab, transform.position, Quaternion.identity)
-                    .GetComponent<LootSpawner>().SpawnLoot(healthPotionDroprate, yarnDroprate);
-                lootDropped = true;
-                
-            }
+            StartCoroutine(OpenChest());
         }
+        // Else, close chest
         else {
             chest.sprite = closedChest;
         }
 
         // audSource.mute = true;
     }
+
+
+
+    // Coroutine to do stuff when open a chest
+    private IEnumerator OpenChest() {
+        audSource.Play();
+
+        // Camera focus on chest
+        mainCamera.GetComponent<CameraControl>().SwitchToObjectFocus(this.transform.position);
+        Debug.Log("Should focus on chest");
+
+        chest.sprite = openChest;
+        // anim.Play("Chest_open");
+
+        if (lootDropped == false) {
+            Instantiate(lootSpawnerPrefab, transform.position, Quaternion.identity)
+                .GetComponent<LootSpawner>().SpawnLoot(healthPotionDroprate, yarnDroprate);
+            lootDropped = true;
+        }
+
+        yield return new WaitForSeconds(3.0f);
+        mainCamera.GetComponent<CameraControl>().SwitchToPlayerFocus();
+    }
+
+
+
+
+  
+
+
+
+
 
 }
