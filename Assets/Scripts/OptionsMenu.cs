873686db67483050214ef36fdb1763654b680f2b
anaperezz <102556053+anaperezz@users.noreply.github.com>
2023-11-20 02:11:06 -0500

    added volume carrying over between scenes

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -11,22 +11,32 @@ public class OptionsMenu : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
-    private void Awake(){
-        SetMusicVolume();
-        SetSFXVolume();
-    }
     private void Start(){
-        SetMusicVolume();
-        SetSFXVolume();
+        if(PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("sfxVolume")){
+            LoadVolume();
+        }
+        else{
+            SetMusicVolume();
+            SetSFXVolume();
+        }
     }
 
     public void SetMusicVolume(){
         float volume = musicSlider.value;
         audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
     public void SetSFXVolume(){
         float volume = sfxSlider.value;
         audioMixer.SetFloat("sfx", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    public void LoadVolume(){
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetMusicVolume();
+        SetSFXVolume();
     }
 
 }
