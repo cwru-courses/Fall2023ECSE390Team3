3ec04863f250c32f5664735b91b028b9f04526a6
Zoe Goldberg <zgoldberg22@gmail.com>
2023-11-21 15:24:25 -0600

    fixed dialogue

--- Assets/Scripts/OpeningDialogue.cs
+++ Assets/Scripts/OpeningDialogue.cs
@@ -19,14 +19,13 @@ public class OpeningDialogue : CollisionDialogue
         //     // health_arrow.SetActive(false);
         //     // yarn_arrow.SetActive(true);
         // } 
-        // if(base.index == 5){
-        //     // yarn_arrow.SetActive(false);
-        //     // This icon shows you how much time you have to wait until your next stitch.
-        //     shift_arrow.SetActive(true);
-        // }
-        // else if(base.index > 5){
-        //     shift_arrow.SetActive(false);
-        // }
+        if(base.index == 1){
+            // yarn_arrow.SetActive(false);
+            shift_arrow.SetActive(true);
+        }
+        else if(base.index > 1){
+            shift_arrow.SetActive(false);
+        }
         foreach(char c in base.lines[base.index].ToCharArray()){
             base.textComponent.text += c;
             if(c != ' '){
