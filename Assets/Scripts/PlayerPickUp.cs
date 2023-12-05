a258cbcb4b59eba9acc84ac0efbac6b088080a81
lsn19 <lsn19@case.edu>
2023-12-05 07:39:26 -0500

    fixed pickupUI

--- Assets/Scripts/PlayerPickUp.cs
+++ Assets/Scripts/PlayerPickUp.cs
@@ -14,6 +14,9 @@ public class PlayerPickUp : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI PickUpIcon;
 
+    [SerializeField] private InputRebind inputRebind;
+
+
     void Start()
     {
     }
@@ -26,12 +29,12 @@ public class PlayerPickUp : MonoBehaviour
 
         if (pickedUpObject)
         {
-            PickUpIcon.text = "Press P to put down"; //change P to user input
+            PickUpIcon.text = "Press " + inputRebind.GetBoundKey() + " to put down"; 
         }
 
         else if (objInRadius)
         {
-                PickUpIcon.text = "Press P to pick up"; //change P to user input
+                PickUpIcon.text = "Press " + inputRebind.GetBoundKey() + " to pick up"; 
         }
 
         else
