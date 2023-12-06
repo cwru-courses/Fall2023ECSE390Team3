dadfeaf7fed6847e55b3ed7623bcd6ebeda1279d
anaperezz <102556053+anaperezz@users.noreply.github.com>
2023-12-06 01:07:33 -0500

    implemented button sfx

---
+++ Assets/AudioController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+
+public class AudioController : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] private AudioSource hoverSFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        hoverSFX.Play();
+    }
+}
